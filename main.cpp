e19d074e8f8e26772898e3ae158606b5697612c8
itzAlexP <aperezbcn94@mail.com>
2018-01-08 12:59:08 -0800

    Finalizada entrega

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include <vector>
 #include <pugixml.hpp>
 #include <SFML/Network.hpp>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
 #define HOST "tcp://127.0.0.1:3306"
 #define USER "root"
@@ -32,7 +36,7 @@ char
 cBufferSocket[100];
 
 sf::TcpListener dispatcher;
-sf::TcpSocket socket;
+sf::TcpSocket* socket = new sf::TcpSocket;
 sf::Socket::Status status;
 
 int
@@ -41,6 +45,12 @@ iIdRaza;
 
 size_t
 received;
+
+struct sockets
+{
+    std::vector <sf::TcpSocket*> socketList;
+};
+
 
 
 std::string
@@ -61,15 +71,47 @@ std::vector<string>
 sNameRaces,
 sNameCharacters;
 
+
+
 bool
 bPlayerCreated = false,
 bRepeatPassword = false,
 bNewUserVerified = false,
 bRaceCreated = false,
 bCharacterCreated = false,
-bCloseServer = false,
+bCloseGame = false,
 bCharacterNameVerified = false,
 bCharacterSelected = false;
+
+void avisoLogin(struct sockets* sockerinos)
+{
+
+    std::string sLogin = "info-Se ha conectado: " + sChoosedCharacter;
+
+    for(int i = 0; i < sockerinos->socketList.size(); i++)
+    {
+
+        sockerinos->socketList[i]->send(sLogin.c_str(), sLogin.size() + 1);
+
+    }
+
+
+}
+void avisoLogout(struct sockets* sockerinos)
+{
+
+
+    std::string sLogout = "info-Se ha conectado: " + sChoosedCharacter;
+
+    for(int i = 0; i < sockerinos->socketList.size(); i++)
+    {
+
+        sockerinos->socketList[i]->send(sLogout.c_str(), sLogout.size() + 1);
+
+    }
+
+
+}
 
 void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
 {
@@ -91,14 +133,17 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
 
     }
 
-    status = socket.send(sDataSenderAux.c_str(), sDataSenderAux.size() + 1);
+    //Liberamos res
+    delete(res);
+
+    status = socket->send(sDataSenderAux.c_str(), sDataSenderAux.size() + 1);
 
     //Esperamos raza del jugador
-    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+    status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
     sUserRaces = cBufferSocket;
 
     //Esperamos nombre del personaje
-    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+    status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
     sCharacterName = cBufferSocket;
 
     //Comprobamos si el nombre esta libre
@@ -107,6 +152,9 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
     if(res->next() && res->getInt(1) == 1) //Nombre Ocupado
     {
 
+        //Liberamos res
+        delete(res);
+
         //Volvemos a pedir usuario
         while(!bCharacterNameVerified)
         {
@@ -114,14 +162,11 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
             cBufferSocket[1] = '\0';
 
             //Informamos que el nombre ya existe
-            status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+            status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
             //Esperamos respuesta de nuevo nombre
-            status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+            status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
             sCharacterName = cBufferSocket;
-
-            //Liberamos resultset anterior
-            //delete(res);
 
             //Comprobamos si existe usuario con dichos datos
             res = stmt->executeQuery("SELECT count(*) FROM Personajes, Jugadores, Razas WHERE Personajes.IDJugador = Jugadores.JugadorID AND Personajes.IDRaza = Razas.RazaID AND Personajes.Nombre = '" + sCharacterName + "'");
@@ -130,10 +175,14 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
                 cBufferSocket[0] = '0';
                 cBufferSocket[1] = '\0';
 
-                status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                 bCharacterNameVerified = true;
 
             }
+
+            //Liberamos res
+            delete(res);
+
         }
     }
     else
@@ -142,7 +191,7 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
         cBufferSocket[0] = '0';
         cBufferSocket[1] = '\0';
 
-        status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+        status = socket->send(cBufferSocket, sizeof(cBufferSocket));
         bCharacterNameVerified = true;
 
 
@@ -157,6 +206,9 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
     }
 
 
+    //Liberamos res
+    delete(res);
+
     //Obtenemos id de la raza
     res = stmt->executeQuery("SELECT RazaId FROM Razas WHERE Nombre = '"+sUserRaces+"'");
     while(res->next())
@@ -164,6 +216,10 @@ void CrearPersonaje(sql::ResultSet* res, sql::Statement* stmt)
 
         iIdRaza = res->getInt("RazaID");
     }
+
+
+    //Liberamos res
+    delete(res);
 
     //Realizamos insert del personaje
     stmt->execute("INSERT INTO Personajes(Nombre, IDJugador, IDRaza) VALUES ('"+sCharacterName+"', "+ std::to_string(iIdJugador) +", "+ std::to_string(iIdRaza) +")");
@@ -184,35 +240,39 @@ void ingame()
 
 
     //Hasta que no escriba quit no paramos el juego
-    while(sPlayerSelection != "quit") {
+    while(sPlayerSelection != "quit")
+    {
 
-    //Enviamos descripcion
-    sDataSender = currentNode.child_value("descripcion");
-    status = socket.send(sDataSender.c_str(), sizeof(cBufferSocket));
+        //Enviamos descripcion
+        //sDataSender = "noInfo,";
+        sDataSender = currentNode.child_value("descripcion");
+        status = socket->send(sDataSender.c_str(), sizeof(cBufferSocket));
 
-    //Enviamos zonas
-    sDataSender = "";
-    for(pugi::xml_node direccion = currentNode.child("conexiones"); direccion; direccion = direccion.next_sibling("conexiones")){
+        //Enviamos zonas
+        sDataSender = "";
+        for(pugi::xml_node direccion = currentNode.child("conexiones"); direccion; direccion = direccion.next_sibling("conexiones"))
+        {
 
 
-    if(direccion.child_value("Norte") != "")sDataSender = sDataSender + "Norte-";
-    if(direccion.child_value("Sur") != "")sDataSender = sDataSender + "Sur-";
-    if(direccion.child_value("Este") != "")sDataSender = sDataSender + "Este-";
-    if(direccion.child_value("Oeste") != "")sDataSender = sDataSender + "Oeste-";
+            if(direccion.child_value("Norte") != "")sDataSender = sDataSender + "Norte-";
+            if(direccion.child_value("Sur") != "")sDataSender = sDataSender + "Sur-";
+            if(direccion.child_value("Este") != "")sDataSender = sDataSender + "Este-";
+            if(direccion.child_value("Oeste") != "")sDataSender = sDataSender + "Oeste-";
+
+        }
+
+        //Enviamos direcciones validas
+        status = socket->send(sDataSender.c_str(), sizeof(cBufferSocket));
+
+        //Esperamos seleccion del jugador
+        status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
+        sPlayerSelection = cBufferSocket;
+        currentNode = doc.child("mazmorra").find_child_by_attribute("habitacion", "id", currentNode.child("conexiones").child_value(sPlayerSelection.c_str()));
+
 
     }
 
-    //Enviamos direcciones validas
-    status = socket.send(sDataSender.c_str(), sizeof(cBufferSocket));
-
-    //Esperamos seleccion del jugador
-    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
-    sPlayerSelection = cBufferSocket;
-    currentNode = doc.child("mazmorra").find_child_by_attribute("habitacion", "id", currentNode.child("conexiones").child_value(sPlayerSelection.c_str()));
-
-
-    }
-
+    bCloseGame = true;
 }
 
 void gameloop()
@@ -231,7 +291,7 @@ void gameloop()
 
 
         //El proceso esperara la recepcion del usuario
-        sf::Socket::Status status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+        sf::Socket::Status status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
         sUserNick = cBufferSocket;
 
         //Comprobamos si existe el nick del jugador
@@ -240,19 +300,20 @@ void gameloop()
         if(res->next() && res->getInt(1) == 1) //Existe usuario
         {
 
+            //Liberamos res
+            delete(res);
+
+
             //Existe usuario
             cBufferSocket[0] = '0';
             cBufferSocket[1] = '\0';
 
             //Informamos que el usuario es correcto
-            status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+            status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
             //Esperamos respuesta de la contraseña
-            status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+            status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
             sUserPass = cBufferSocket;
-
-            //Liberamos resultset anterior
-            //delete(res);
 
             //Comprobamos si existe usuario con dichos datos
             sql::ResultSet* res = stmt->executeQuery("SELECT count(*) FROM Jugadores WHERE Nombre = '" + sUserNick + "' AND Pass = '" + sUserPass + "'");
@@ -260,17 +321,24 @@ void gameloop()
             if(res->next() && res->getInt(1) == 1) //Existe usuario con contraseña
             {
 
+                //Liberamos res
+                delete(res);
+
                 //Existe usuario
                 cBufferSocket[0] = '0';
                 cBufferSocket[1] = '\0';
 
                 //Informamos que el usuario con contraseña es correcto
-                status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
 
             }
             else //No existe usuario con contraseña
             {
+
+                //Liberamos res
+                delete(res);
+
 
                 while(!bPasswordVerified)//Mientras no la escriba bien repetiremos
                 {
@@ -279,22 +347,22 @@ void gameloop()
                     cBufferSocket[1] = '\0';
 
                     //Informamos que el usuario con contraseña es incorrecto
-                    status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                    status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
                     //Esperamos respuesta de la contraseña
-                    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+                    status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
                     sUserPass = cBufferSocket;
-
-                    //Liberamos resultset anterior
-                    //delete(res);
 
                     //Comprobamos si existe usuario con dichos datos
                     sql::ResultSet* res = stmt->executeQuery("SELECT count(*) FROM Jugadores WHERE Nombre = '" + sUserNick + "' AND Pass = '" + sUserPass + "'");
                     if(res->next() && res->getInt(1) == 1) //Existe usuario con contraseña
                     {
+                        //Liberamos res
+                        delete(res);
+
                         cBufferSocket[0] = '0';
                         cBufferSocket[1] = '\0';
-                        status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                        status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                         bPasswordVerified = true;
                     }
 
@@ -305,6 +373,9 @@ void gameloop()
         }
         else //No existe el usuario
         {
+            //Liberamos res
+            delete(res);
+
             while(!bPlayerCreated)//Mientras no este el jugador creado repetiremos
             {
 
@@ -313,22 +384,22 @@ void gameloop()
                 cBufferSocket[1] = '\0';
 
                 //Informamos que el usuario no existe
-                status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
 
                 //Recibimos nombre de usuario
-                status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+                status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
 
                 sUserNick = cBufferSocket;
-
-                //Liberamos resultset anterior
-                //delete(res);
 
                 //Comprobamos si el nick esta libre
                 sql::ResultSet* res = stmt->executeQuery("SELECT count(*) FROM Jugadores WHERE Nombre = '" + sUserNick + "'");
 
                 if(res->next() && res->getInt(1) == 1) //Existe usuario
                 {
+
+                    //Liberamos res
+                    delete(res);
 
                     //Volvemos a pedir usuario
                     while(!bNewUserVerified)
@@ -337,23 +408,23 @@ void gameloop()
                         cBufferSocket[1] = '\0';
 
                         //Informamos que el usuario ya existe
-                        status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                        status = socket->send(cBufferSocket, sizeof(cBufferSocket));
 
                         //Esperamos respuesta de nuevo nombre de usuario
-                        status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+                        status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
                         sUserNick = cBufferSocket;
-
-                        //Liberamos resultset anterior
-                        //delete(res);
 
                         //Comprobamos si existe usuario con dichos datos
                         sql::ResultSet* res = stmt->executeQuery("SELECT count(*) FROM Jugadores WHERE Nombre = '" + sUserNick + "'");
                         if(res->next() && res->getInt(1) != 1) //No existe usuario
                         {
+                            //Liberamos res
+                            delete(res);
+
                             cBufferSocket[0] = '0';
                             cBufferSocket[1] = '\0';
 
-                            status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                            status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                             bNewUserVerified = true;
                         }
                     }
@@ -361,20 +432,23 @@ void gameloop()
                 else
                 {
 
+                    //Liberamos res
+                    delete(res);
+
                     //Informamos al servidor que el nombre esta libre
                     cBufferSocket[0] = '0';
                     cBufferSocket[1] = '\0';
-                    status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                    status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                 }
 
-                while(!bRepeatPassword)
+                while(!bRepeatPassword)//Repetiremos hasta que coincidan las contraseñas
                 {
 
 
-                    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+                    status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
                     sUserPass = cBufferSocket;
 
-                    status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+                    status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
                     sUserPassRepeat = cBufferSocket;
 
                     if(sUserPass == sUserPassRepeat)//Las contraseñas coinciden
@@ -382,7 +456,7 @@ void gameloop()
                         cBufferSocket[0] = '0';
                         cBufferSocket[1] = '\0';
 
-                        status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                        status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                         stmt->execute("INSERT INTO Jugadores (Nombre, Pass) VALUES ('"+ sUserNick +"', '"+ sUserPass +"')");
                         bRepeatPassword = true;
                         bPlayerCreated = true;
@@ -392,7 +466,7 @@ void gameloop()
                         cBufferSocket[0] = '1';
                         cBufferSocket[1] = '\0';
 
-                        status = socket.send(cBufferSocket, sizeof(cBufferSocket));
+                        status = socket->send(cBufferSocket, sizeof(cBufferSocket));
                     }
                 }
 
@@ -409,7 +483,6 @@ void gameloop()
         while(!bCharacterSelected)
         {
 
-
             //Obtenemos id del jugador
             res = stmt->executeQuery("SELECT JugadorId FROM Jugadores WHERE Nombre = '"+sUserNick+"'");
             while(res->next())
@@ -417,6 +490,9 @@ void gameloop()
 
                 iIdJugador = res->getInt("JugadorID");
             }
+
+            //Liberamos res
+            delete(res);
 
             res = stmt->executeQuery("SELECT Personajes.Nombre AS pNombre, Razas.Nombre AS rNombre FROM Personajes, Jugadores, Razas WHERE Personajes.IDJugador = Jugadores.JugadorID AND Personajes.IDRaza = Razas.RazaID AND Personajes.IDJugador = "+std::to_string(iIdJugador)+"");
 
@@ -427,11 +503,14 @@ void gameloop()
 
             }
 
+            //Liberamos res
+            delete(res);
+
             //Enviamos listado
-            status = socket.send(sDataSender.c_str(), sDataSender.size() + 1);
+            status = socket->send(sDataSender.c_str(), sDataSender.size() + 1);
 
             //Esperamos respuesta del nombre
-            status = socket.receive(cBufferSocket, sizeof(cBufferSocket), received);
+            status = socket->receive(cBufferSocket, sizeof(cBufferSocket), received);
 
 
             //El jugador desea crear un nuevo personaje
@@ -451,6 +530,10 @@ void gameloop()
             }
         }
 
+        //Liberamos statment y conexion
+        delete(stmt);
+        delete(con);
+
     }
     catch(sql::SQLException &e)
     {
@@ -463,13 +546,20 @@ void gameloop()
 int main()
 {
 
+
+
+
+
     //Iniciamos el listener para aceptar conexiones
     status = dispatcher.listen(5000);
 
+    //Obteneemos la key de memoria compartida
+    int idSharedMemory = shmget(IPC_PRIVATE, sizeof(struct sockets), IPC_CREAT|0666);
+    struct sockets* ptrSharedMemory = (struct sockets*) shmat(idSharedMemory, NULL, 0);
 
     //Haremos que el socket y el dispatcher no bloquen el hilo principal
     dispatcher.setBlocking(false);
-    socket.setBlocking(false);
+    socket->setBlocking(false);
 
 
     if(status != sf::Socket::Done)
@@ -487,29 +577,39 @@ int main()
     }
 
     //El servidor permanecera activo hasta que lo queramos apagar
-    while(!bCloseServer)
+    while(!bCloseGame)
     {
 
         //Comprobamos si hay conexiones.
-        if(dispatcher.accept(socket) == sf::Socket::Done)
+        if(dispatcher.accept(*socket) == sf::Socket::Done)
         {
 
-            //Creamos un proceso para cada conexion.
+            //Introducimos la conexion en el vector de memoria compartida
+            ptrSharedMemory->socketList.push_back(socket);
 
+            //Creamos un proceso para cada conexion.
             pNuevoUsuario = fork();
 
             if(pNuevoUsuario == 0)
             {
                 //Para el formulario haremos que el socket se bloque esperando respuesta del cliente
-                socket.setBlocking(true);
+                socket->setBlocking(true);
+
+                //Registro/Login
                 gameloop();
+
+                //Informamos que se ha conectado alguien
+                //avisoLogin(ptrSharedMemory);
+
+                //Iniciamos el juego
                 ingame();
+
+                //Informamos de la desconexion de alguien
+                //avisoLogout(ptrSharedMemory);
 
             }
         }
     }
-
-
 
     return 0;
 }
